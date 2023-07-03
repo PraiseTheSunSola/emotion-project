cf404bf66b9bf238d309942cf403ce7d55d427a6
PraiseTheSunSola <pledgemo2115@gmail.com>
2023-07-03 16:54:13 +0900

    내용 추가

--- src/Project/Regulation.js
+++ src/Project/Regulation.js
@@ -5,7 +5,14 @@ const Page = styled.div``;
 export function Regulation() {
   return (
     <>
-      <Page></Page>
+      <Page>
+        <button />
+        <buttons>
+          <button1></button1>
+          <button2></button2>
+          <button3></button3>
+        </buttons>
+      </Page>
     </>
   );
 }
