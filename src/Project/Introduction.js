42df552d4fc6994f78ffc22ad8becf22663f3f54
PraiseTheSunSola <pledgemo2115@gmail.com>
2023-07-07 17:42:38 +0900

    내용 추가

--- src/Project/Introduction.js
+++ src/Project/Introduction.js
@@ -1,11 +1,35 @@
 import styled from "styled-components";
 import EmotionDB from "./EmotionDB";
-import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
+import { AnimatePresence, motion, useAnimation } from "framer-motion";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-// ------------------------------------------- 버튼
+const Page = styled.div`
+  width: 100vw;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+`;
 
+const DbBox = styled.div`
+  width: 300px;
+  height: 300px;
+  margin-bottom: 20px;
+  text-align: center;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Name = styled.h3`
+  font-size: 20px;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 80%;
+`;
+
+// ------------------------------------------- 버튼
 const NavButtonCircle = styled(motion.div)`
   position: relative;
   position: fixed;
@@ -57,35 +81,15 @@ const inout = {
   end: { opacity: 1, scale: 1, rotateZ: 0, transition: { type: "spring" } },
   exit: { opacity: 0, scale: 0, rotateZ: 0 },
 };
-
 // ------------------------------------------- 버튼
 
-const Page = styled.div`
-  width: 100vw;
-  display: grid;
-
-  grid-template-columns: repeat(5, 1fr);
-`;
-
-const DbBox = styled.div`
-  text-align: center;
-  width: 300px;
-  height: 300px;
-  margin-bottom: 20px;
-  /* border: 5px solid black; */
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const Name = styled.h3``;
-
-const Img = styled.img`
-  width: 100%;
-  height: 80%;
-`;
-
 export function Introduction() {
+  const controls = useAnimation();
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+  useEffect(() => {
+    controls.start({ x: "100%" }); // 초기 위치 설정
+  }, [controls]);
+
   const [show, setShow] = useState(false);
   function clickButton() {
     setShow((appear) => !appear);
@@ -95,10 +99,28 @@ export function Introduction() {
     <>
       <Page>
         {EmotionDB.map((Emotion, i) => (
-          <DbBox key={Emotion.id}>
-            <Img src={Emotion.image} alt={Emotion.description} />
-            <Name>{Emotion.title}</Name>
-          </DbBox>
+          <motion.div
+            style={{ width: "100%", height: "80%" }}
+            key={Emotion.id}
+            initial={{ x: i % 2 === 0 ? "100%" : "-100%" }}
+            animate={
+              hoveredIndex === i
+                ? { x: 0 }
+                : { x: i % 2 === 0 ? "-100%" : "100%" }
+            }
+            transition={{ duration: 10, repeat: Infinity }} // 애니메이션의 지속 시간
+            onMouseEnter={() => setHoveredIndex(i)}
+            onMouseLeave={() => setHoveredIndex(null)}
+          >
+            <DbBox>
+              <motion.img
+                src={Emotion.image}
+                alt={Emotion.description}
+                style={{ width: "100%", height: "80%" }}
+              />
+              <Name>{Emotion.title}</Name>
+            </DbBox>
+          </motion.div>
         ))}
       </Page>
 
