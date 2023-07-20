b29fa6e9c88b1a0db6f9af782f3dcf05445fe766
PraiseTheSunSola <pledgemo2115@gmail.com>
2023-07-20 09:21:20 +0900

    내용 추가

--- src/Project/EmotionDB.js
+++ src/Project/EmotionDB.js
@@ -18,6 +18,46 @@ import miserable from "../Emotions/17miserable.jpg";
 import lethargy from "../Emotions/18lethargy.jpg";
 import despondency from "../Emotions/19despondency.jpg";
 import anguish from "../Emotions/20anguish.jpg";
+import APPREHENSION from "../Emotions/21apprehension.jpg"
+import FEAR  from "../Emotions/22fear.jpg";
+import WORRY from "../Emotions/23worry.jpg";
+import ANXIOUS from "../Emotions/24anxious.jpg";
+import JEALOUSY from "../Emotions/25Jealousy.png";
+import DOUBT from "../Emotions/26doubt.jpg";
+import WEAKNESS from "../Emotions/27weakness.jpg";
+import PERPLEXITY from "../Emotions/28perplexity.jpg";
+import HURT from "../Emotions/29Hurt.jpg";
+import HOMESICKNESS from "../Emotions/30homesickness.jpg";
+import BETRAYAL from "../Emotions/31betrayal.jpg";
+import SOLITUDE from "../Emotions/32solitude.jpg";
+import BEWILDERMENT from "../Emotions/33bewilderment.jpg";
+import INJUSTICE from "../Emotions/34injustice.jpg";
+import SUFFERING from "../Emotions/35suffering.jpg";
+import UNHAPPINESS from "../Emotions/36unhappiness.jpg";
+import SHAME from "../Emotions/37shame.jpg";
+import INFERIORITYCOMPLEX from "../Emotions/38inferioritycomplex.jpg";
+import GUILT from "../Emotions/39guilt.jpg";
+import DISPLEASURE from "../Emotions/40displeasure.jpg";
+import PITIFUL from "../Emotions/41pitiful.jpg";
+import JOY from "../Emotions/42joy.jpg";
+import GRATITUDE from "../Emotions/43gratitude.png";
+import COMFORT from "../Emotions/44comfort.jpg";
+import ELATION from "../Emotions/45elation.jpg";
+import HAPPINESS from "../Emotions/46happiness.jpg";
+import FAITH from "../Emotions/47faith.jpg";
+import SERENITY from "../Emotions/48serenity.jpg";
+import PLEASURE from "../Emotions/49pleasure.jpg";
+import COURAGE from "../Emotions/50courage.jpg";
+import RELIEF from "../Emotions/51Relief.jpg";
+import LOVE from "../Emotions/52love.jpg";
+import SenseOfBelonging from "../Emotions/53senseofbelonging.jpg";
+import FRIENDSHIP from "../Emotions/54Friendship.jpg";
+import DEVOTION from "../Emotions/55Devotion.jpg";
+import IMPRESSION from "../Emotions/56impression.png";
+import MERCY from "../Emotions/57Mercy.jpg";
+import Kindness from "../Emotions/58Kindness.jpg";
+
+
 const EmotionKind = [
   {
     id: 1,
@@ -145,6 +185,273 @@ const EmotionKind = [
     title: "ANGUISH (고통)",
     image: anguish,
     text: "극도로 심한 고통이나 괴로움을 의미",
+  },
+
+  {
+    id: 21,
+    title: "APPREHENSION (불안)",
+    image: APPREHENSION ,
+    text: "불확실한 상황에 대한 불안 또는 두려움을 의미",
+  },
+
+  {
+    id: 22,
+    title: "FEAR (두려움)",
+    image: FEAR ,
+    text: "위협적이거나 불안하거나 위험한 상황에 대한 감정으로 인해 발생하는 불안한 감정",
+  },
+  
+  {
+    id: 23,
+    title: "WORRY (걱정)",
+    image: WORRY ,
+    text: "불안 또는 불편한 상태에 대한 마음의 불편한 감정이며 미래의 불확실성에 대한 고민과 염려를 포함",
+  },
+
+  {
+    id: 24,
+    title: "ANXIOUS (초조)",
+    image: ANXIOUS ,
+    text: "불안 또는 불편한 상태에 있는 느낌이며 불안정한 상태로 인해 긴장되어 있는 상태를 나타냄",
+  },
+
+  {
+    id: 25,
+    title: "JEALOUSY (질투)",
+    image: JEALOUSY ,
+    text: "누군가가 누구나 또는 무언가와 비교하여 불평등함을 느끼고, 그로 인해 비애, 불안 또는 분노를 느끼는 감정",
+  },
+  
+  {
+    id: 26,
+    title: "DOUBT (의심)",
+    image: DOUBT ,
+    text: "어떤 사실이나 주장에 대해 불확실하게 믿거나 믿지 않는 상태를 나타내는 감정",
+  },
+  
+  {
+    id: 27,
+    title: "WEAKNESS (약함)",
+    image: WEAKNESS ,
+    text: "육체적 또는 정신적으로 능력이 떨어지거나 취약한 상태",
+  },
+
+  {
+    id: 28,
+    title: "PERPLEXITY (당혹)",
+    image: PERPLEXITY,
+    text: "어떤 상황이나 정보에 대해 이해하기 어렵고 구별하기 힘들어하는 상태",
+  },
+  
+  {
+    id: 29,
+    title: "HURT (상처)",
+    image: HURT ,
+    text: "신체적, 정신적 또는 감정적으로 아픈 상태를 나타내며 상처를 받은 감정",
+  },
+  
+  
+  {
+    id: 30,
+    title: "HOMESICKNESS (그리움)",
+    image: HOMESICKNESS ,
+    text: "가족, 친구 또는 익숙한 장소에서 떨어져있을 때 고향이나 집에 대한 그리움을 뜻함",
+  },
+
+  {
+    id: 31,
+    title: "BETRAYAL (배신)",
+    image: BETRAYAL,
+    text: "믿었던 사람이나 단체에 대해 배신당한 경험으로 인해 느끼는 강한 실망과 상처를 나타냄",
+  },
+  
+  {
+    id: 32,
+    title: "SOLITUDE (고독)",
+    image: SOLITUDE ,
+    text: "혼자 있는 상태이거나 사람들과 떨어져 있는 시간 혹은 상태를 의미",
+  },
+
+  {
+    id: 33,
+    title: "BEWILDERMENT (어리둥절)",
+    image: BEWILDERMENT,
+    text: "혼란스러움으로 인해 방향을 잃은 상태",
+  },
+
+  {
+    id: 34,
+    title: "INJUSTICE (억울함)",
+    image: INJUSTICE ,
+    text: "공정하지 않거나 불공평한 대우를 받았을 때의 분노나 불만을 나타냄",
+  },
+  
+  {
+    id: 35,
+    title: "SUFFERING (괴로움)",
+    image: SUFFERING ,
+    text: "고통이나 괴로움을 느끼는 감정으로, 불행하거나 힘든 상황에 처해있을 때 느끼는 것",
+  },
+  
+  {
+    id: 36,
+    title: "UNHAPPINESS (불행)",
+    image: UNHAPPINESS ,
+    text: "행복하지 않거나 만족하지 못하는 상태",
+  },
+
+  {
+    id: 37,
+    title: "SHAME (수치심)",
+    image: SHAME ,
+    text: "자신의 행동이나 특성으로 인해 느끼는 창피함 또는 불쾌함",
+  },
+  
+  {
+    id: 38,
+    title: "INFERIORITY COMPLEX (열등감)",
+    image: INFERIORITYCOMPLEX,
+    text: "자신보다 낮다고 느끼거나 소외되는 것과 관련하여 강한 불안감을 가지는 상태",
+  },
+  
+  {
+    id: 39,
+    title: "GUILT (죄책감)",
+    image: GUILT  ,
+    text: "어떤 잘못을 저질렀거나 타인에게 해를 끼친 것에 대한 마음의 불편한 감정",
+  },
+
+  {
+    id: 40,
+    title: "DISPLEASURE (불쾌함)",
+    image: DISPLEASURE ,
+    text: "불편하거나 기분이 좋지 않은 상태",
+  },
+  
+  {
+    id: 41,
+    title: "PITIFUL (불쌍함)",
+    image: PITIFUL ,
+    text: "다른 사람의 고통이나 어려움에 대해 동정심이나 불쌍함을 느끼게 만드는 상태",
+  },
+
+  {
+    id: 42,
+    title: "JOY (기쁨)",
+    image: JOY ,
+    text: "큰 만족감과 행복한 감정",
+  },
+
+  {
+    id: 43,
+    title: "GRATITUDE (감사)",
+    image: GRATITUDE ,
+    text: "누군가에 대해 인정하고 감사하는 감정",
+  },
+  
+  {
+    id: 44,
+    title: "COMFORT (위로)",
+    image: COMFORT ,
+    text: "편안하고 안락한 상태",
+  },
+  
+  {
+    id: 45,
+    title: "ELATION (들뜬)",
+    image: ELATION ,
+    text: "흥분. 행복이나 기쁨으로 인해 들뜬 감정",
+  },
+
+  {
+    id: 46,
+    title: "HAPPINESS (행복)",
+    image: HAPPINESS ,
+    text: "만족하고 기쁜 감정의 상태",
+  },
+  
+  {
+    id: 47,
+    title: "FAITH (믿음)",
+    image: FAITH ,
+    text: "신념이나 믿음으로서 믿는 것에 대한 강한 신념",
+  },
+  
+  {
+    id: 48,
+    title: "SERENITY (평온)",
+    image: SERENITY ,
+    text: "내적으로 평화롭고 안정된 상태",
+  },
+
+  {
+    id: 49,
+    title: "PLEASURE (즐거움)",
+    image: PLEASURE ,
+    text: "기쁨과 만족감을 주는 상태",
+  },
+  
+  {
+    id: 50,
+    title: "COURAGE (용기)",
+    image: COURAGE ,
+    text: "어려움이나 위험에도 불구하고 두려워하지 않고 강하게 직면하는 것",
+  },
+
+  {
+    id: 51,
+    title: "RELIEF (안심)",
+    image: RELIEF ,
+    text: "어려운 상황이나 스트레스로부터 벗어나서 느끼는 안도감",
+  },
+
+  {
+    id: 52,
+    title: "LOVE (사랑)",
+    image: LOVE ,
+    text: "강한 감정으로, 다른 사람이나 사물에 대한 애착과 관심",
+  },
+  
+  {
+    id: 53,
+    title: "sense of belonging (유대감)",
+    image: SenseOfBelonging ,
+    text: "사람들 간에 강한 연결과 유대성을 느끼는 감정",
+  },
+  
+  {
+    id: 54,
+    title: "FRIENDSHIP (우정)",
+    image: FRIENDSHIP ,
+    text: "서로의 친밀한 관계와 호감을 나타내는 사이",
+  },
+
+  {
+    id: 55,
+    title: "DEVOTION (헌신)",
+    image: DEVOTION ,
+    text: "다른 사람이나 목표에 대한 헌신과 충성",
+  },
+  
+  {
+    id: 56,
+    title: "IMPRESSION (감동)",
+    image: IMPRESSION ,
+    text: "사람들이 다양한 상황에서 느끼는 감정이나 감회를 의미",
+  },
+
+  {
+    id: 57,
+    title: "MERCY (자비)",
+    image: MERCY ,
+    text: "다른 사람에 대해 가혹하지 않고 인정하는 마음",
+  },
+
+  {
+    id: 58,
+    title: "KINDNESS (상냥함)",
+    image: Kindness ,
+    text: "다른 사람에 대해 배려심과 따뜻한 마음",
   },
 ];
 
